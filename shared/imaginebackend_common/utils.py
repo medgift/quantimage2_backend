cacd03461cc27363489faefb2fd274a5a6359b69
Roger Schaer <roger.schaer@hevs.ch>
2019-08-13 11:38:59 +0200

    Handle non-existent result when formatting features

--- shared/imaginebackend_common/utils.py
+++ shared/imaginebackend_common/utils.py
@@ -1,2 +1,7 @@
 def task_status_message(task_result):
+
+    # If task result has disappeared, return empty string (it's complete anyway)
+    if task_result is None:
+        return ""
+
     return f"{task_result['current']}/{task_result['total']} - {task_result['status_message']}"
