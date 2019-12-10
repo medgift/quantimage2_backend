fdc863e12c1eb6e17bf3bb09b61c9e476560e4eb
Roger Schaer <roger.schaer@hevs.ch>
2019-12-10 15:22:09 +0100

    Update extraction task

--- workers/tasks.py
+++ workers/tasks.py
@@ -2,25 +2,22 @@ import os, logging
 import shutil
 import tempfile
 import json
+import traceback
 
 import jsonpickle
 import requests
 
 import warnings
 
+from requests_toolbelt import NonMultipartContentTypeException
+
 warnings.filterwarnings("ignore", message="Failed to parse headers")
 
 from celery import Celery
-from celery.contrib import rdb
-from celery.exceptions import Ignore
 from requests_toolbelt.multipart import decoder
 from pathlib import Path
 
 from config_worker import dicomFields, endpoints
-
-from radiomics import featureextractor
-
-from okapy.dicomconverter.dicom_walker import DicomWalker
 
 from imaginebackend_common.feature_backends import feature_backends_map
 
@@ -32,7 +29,9 @@ celery = Celery(
 
 
 @celery.task(name="imaginetasks.extract", bind=True)
-def run_extraction(self, token, feature_id, study_uid, features_path, config_path):
+def run_extraction(
+    self, token, feature_extraction_task_id, study_uid, features_path, config_path
+):
 
     try:
         current_step = 1
@@ -40,7 +39,9 @@ def run_extraction(self, token, feature_id, study_uid, features_path, config_pat
 
         # Status update - DOWNLOAD
         status_message = "Fetching DICOM files"
-        update_progress(self, feature_id, current_step, steps, status_message)
+        update_progress(
+            self, feature_extraction_task_id, current_step, steps, status_message
+        )
 
         # Get the list of instance URLs
         url_list = get_dicom_url_list(token, study_uid)
@@ -52,7 +53,7 @@ def run_extraction(self, token, feature_id, study_uid, features_path, config_pat
             self,
             dicom_dir,
             config_path,
-            feature_id=feature_id,
+            feature_extraction_task_id=feature_extraction_task_id,
             current_step=current_step,
             steps=steps,
         )
@@ -66,43 +67,48 @@ def run_extraction(self, token, feature_id, study_uid, features_path, config_pat
         Path(features_path).write_text(json_features)
 
         # Extraction is complete
-        status_message = "Extraction COMPLETE"
+        status_message = "Extraction Complete"
 
         return {
-            "feature_id": feature_id,
+            "feature_extraction_task_id": feature_extraction_task_id,
             "current": steps,
             "total": steps,
             "status_message": status_message,
         }
     except Exception as e:
 
-        logging.error("!!!!!AYAYAYAYAYYYYYYYYY")
-        logging.error(e)
-        logging.error("AYAYAYAYAYYYYYYYYY!!!!!")
+        # logging.error(e)
 
-        # current_step = 0
-        # status_message = "Failure!"
-        # print(f"Feature {feature_id} - {current_step}/{steps} - {status_message}")
-        #
-        # meta = {
-        #     "exc_type": type(e).__name__,
-        #     "exc_message": traceback.format_exc().split("\n"),
-        #     "feature_id": feature_id,
-        #     "current": current_step,
-        #     "total": steps,
-        #     "status_message": status_message,
-        # }
-        #
-        # update_task_state(self, "FAILURE", meta)
+        current_step = 0
+        status_message = "Failure!"
+        print(
+            f"Feature extraction task {feature_extraction_task_id} - {current_step}/{steps} - {status_message}"
+        )
+
+        meta = {
+            "exc_type": type(e).__name__,
+            "exc_message": traceback.format_exc().split("\n"),
+            "feature_extraction_task_id": feature_extraction_task_id,
+            "current": current_step,
+            "total": steps,
+            "status_message": status_message,
+        }
+
+        update_task_state(self, "FAILURE", meta)
 
         raise e
 
 
-def update_progress(task, feature_id, current_step, steps, status_message):
-    print(f"Feature {feature_id} - {current_step}/{steps} - {status_message}")
+def update_progress(
+    task, feature_extraction_task_id, current_step, steps, status_message
+):
+    print(
+        f"Feature extraction task {feature_extraction_task_id} - {current_step}/{steps} - {status_message}"
+    )
 
     meta = {
-        "feature_id": feature_id,
+        "task_id": task.request.id,
+        "feature_extraction_task_id": feature_extraction_task_id,
         "current": current_step,
         "total": steps,
         "status_message": status_message,
@@ -175,9 +181,13 @@ def download_dicom_urls(token, urls):
 
         logging.getLogger("urllib3").setLevel(logging.ERROR)
         response = requests.get(url, headers=access_token)
-        # open(os.path.join(tmp_dir, filename), "wb").write(response.content)
 
-        multipart_data = decoder.MultipartDecoder.from_response(response)
+        try:
+            multipart_data = decoder.MultipartDecoder.from_response(response)
+        except NonMultipartContentTypeException as e:
+            print("Got a weird thing from Kheops, response was")
+            print(response.text)
+            raise e
 
         # Get first part, with the content
         part = multipart_data.parts[0]
@@ -190,7 +200,12 @@ def download_dicom_urls(token, urls):
 
 
 def extract_all_features(
-    task, dicom_dir, config_path, feature_id=None, current_step=None, steps=None
+    task,
+    dicom_dir,
+    config_path,
+    feature_extraction_task_id=None,
+    current_step=None,
+    steps=None,
 ):
     config = json.loads(Path(config_path).read_text()) if config_path else None
 
@@ -199,7 +214,9 @@ def extract_all_features(
     # Status update - CONVERT
     current_step += 1
     status_message = "Pre-processing data"
-    update_progress(task, feature_id, current_step, steps, status_message)
+    update_progress(
+        task, feature_extraction_task_id, current_step, steps, status_message
+    )
 
     backend_inputs = {}
     for backend in config["backends"]:
@@ -212,7 +229,9 @@ def extract_all_features(
     # Status update - EXTRACT
     current_step += 1
     status_message = "Extracting features"
-    update_progress(task, feature_id, current_step, steps, status_message)
+    update_progress(
+        task, feature_extraction_task_id, current_step, steps, status_message
+    )
 
     for backend in config["backends"]:
         # if there is pre-processed data for this backend
