da8cd5941bcada430bcab13c12da064a47d234b3
Roger Schaer <roger.schaer@hevs.ch>
2019-12-10 15:16:26 +0100

    Move method out of shared utils

--- shared/imaginebackend_common/utils.py
+++ shared/imaginebackend_common/utils.py
@@ -1,12 +1,3 @@
-def task_status_message(task_result):
-
-    # If task result has disappeared, return empty string (it's complete anyway)
-    if task_result is None or "status_message" not in task_result:
-        return ""
-
-    return f"{task_result['current']}/{task_result['total']} - {task_result['status_message']}"
-
-
 class CustomException(Exception):
     status_code = 500
 
