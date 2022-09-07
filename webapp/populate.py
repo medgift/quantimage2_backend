dfe0e41d34dc29127c91c3e1d0c3212b41ebbab2
Roger Schaer <roger.schaer@hevs.ch>
2022-09-07 13:10:21 +0200

    Add path mapping for common module in docker-compose override for easier development

--- webapp/populate.py
+++ webapp/populate.py
@@ -4,15 +4,15 @@ from quantimage2_backend_common.models import FeaturePreset
 
 
 def populate_presets():
-    # Copy the PET/CT preset to the right folder
-    print("Populating presets_default")
-
     preset_name = "PET/CT"
 
     # Check if preset exists already
     existing_preset = FeaturePreset.find_by_name(preset_name)
 
     if existing_preset is None:
+        print("Populating presets")
+
+        # Copy the PET/CT preset to the right folder
         preset_file_name = "PETCT_all.yaml"
         preset_src_path = f"presets_default/{preset_file_name}"
         preset_dst_path = f"/quantimage2-data/feature-presets/{preset_file_name}"
