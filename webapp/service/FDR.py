d5b764eceb73a6eb82cfa85a04ff869fbfd4bf91
Poluux <hugoboss82001@hotmail.com>
2026-06-28 18:39:10 +0200

    pvalues are computed for each features

--- webapp/service/FDR.py
+++ webapp/service/FDR.py
@@ -2,6 +2,7 @@ from typing import List
 
 import pandas
 import pandas as pd
+import numpy as np
 
 from service.machine_learning import (
     resolve_collection_clinical_definitions,
@@ -26,6 +27,15 @@ from service.feature_transformation import (
     OUTCOME_FIELD_SURVIVAL_TIME,
 )
 
+from scipy.stats import (
+    shapiro,
+    levene,
+    ttest_ind,
+    mannwhitneyu,
+    chi2_contingency,
+    fisher_exact,
+)
+
 
 def compute_fdr(
     extraction_id,
@@ -41,7 +51,7 @@ def compute_fdr(
     fdr_threshold,
 ):
 
-    features_df, labels_df = assemble_features_for_collection(
+    features_df, labels_df, full_feature_metadata = assemble_features_for_collection(
         extraction_id,
         collection_id,
         album,
@@ -59,9 +69,17 @@ def compute_fdr(
         raise ValueError(f"Selected features not found in dataset: {missing}")
 
     features_df = features_df[selected_feature_ids]
+    full_feature_metadata = {
+        feature: metadata
+        for feature, metadata in full_feature_metadata.items()
+        if feature in selected_feature_ids
+    }
 
-    # BEFORE IMPUTE VALUES FOR RADIOMICS FEATURES
-    # compute univariate tests
+    univariate_results = compute_univariate_tests(
+        features_df, labels_df, full_feature_metadata
+    )
+
+    # compute fdr
 
     return
 
@@ -91,18 +109,12 @@ def assemble_features_for_collection(
 
     labels_df_indexed = labels_df_indexed.apply(pandas.to_numeric)
 
-    print("features_df (radiomics) before appending metadata strategy")
-    print(features_df)
-
-    features_df = features_df.drop(columns=["PatientID"])
-
     radiomics_feature_metadata = {}
     for feature in features_df.columns:
         radiomics_feature_metadata[feature] = {
-            "type": ClinicalFeatureTypes.NUMBER.value,
+            "feat_type": ClinicalFeatureTypes.NUMBER.value,
+            "univariate_category": "continuous",
         }
-
-    # TODO - Impute NA values for radiomics features
 
     all_patients = (
         training_patients + test_patients if test_patients else training_patients
@@ -130,7 +142,7 @@ def assemble_features_for_collection(
     else:
         raise ValueError("Neither clinical nore imaging features where selected")
 
-    return features_df, labels_df_indexed
+    return features_df, labels_df_indexed, full_feature_metadata
 
 
 def get_clinical_features(
@@ -207,12 +219,18 @@ def get_clinical_features(
         strategy = feature_strategies.get(col_name, ClinicalFeatureMissingValues.NONE)
 
         df[col_name] = apply_missing_value_strategy(df[col_name], strategy)
+        univariate_type = get_clinical_feature_univariate_type(
+            df[col_name], clin_feature.feat_type
+        )
 
         df = df[[col_name]]
 
         feature_frames.append(df)
 
-        feature_metadata[col_name] = {"feat_type": clin_feature.feat_type}
+        feature_metadata[col_name] = {
+            "feat_type": clin_feature.feat_type,
+            "univariate_category": univariate_type,
+        }
 
     if not feature_frames:
         return pd.DataFrame(index=radiomics_index)
@@ -228,6 +246,9 @@ def get_clinical_features(
 def apply_missing_value_strategy(series, strategy):
 
     series = series.copy()
+
+    if not series.isna().any():
+        return series
 
     if strategy == ClinicalFeatureMissingValues.MEAN:
         return series.fillna(series.mean())
@@ -252,3 +273,99 @@ def correct_type_of_clinical_values(series: pd.Series, feat_type: str) -> pd.Ser
         series = pd.Series(pd.to_numeric(series, errors="coerce"))
 
     return series
+
+
+def compute_univariate_tests(features_df, labels_df, full_feature_metadata):
+    records = []
+
+    for feature in features_df.columns:
+        test_used, stat, p = select_and_run_univariate_test(
+            features_df[feature],
+            labels_df,
+            full_feature_metadata[feature]["univariate_category"],
+        )
+
+        full_feature_metadata[feature].update(
+            {
+                "test_used": test_used,
+                "statistic": float(stat),
+                "pvalue": float(p),
+            }
+        )
+
+        records.append(
+            {
+                "feature": feature,
+                "test_used": test_used,
+                "statistic": float(stat),
+                "pvalue": float(p),
+            }
+        )
+
+    records_df = pd.DataFrame(records).set_index("feature").sort_values(by="pvalue")
+
+    return records_df
+
+
+def select_and_run_univariate_test(series, targets, feature_value_category, alpha=0.05):
+    targets_series = targets.squeeze()
+    classes = targets_series.unique()
+
+    if feature_value_category == "continuous":
+        group0 = series[targets_series == classes[0]]
+        group1 = series[targets_series == classes[1]]
+
+        is_normal = check_normality(group0, group1, alpha)
+
+        if not is_normal:
+            stat, pvalue = mannwhitneyu(group0, group1)
+            test_used = "mannwhitneyu"
+
+        equal_variance = levene(group0, group1).pvalue > alpha
+
+        if equal_variance:
+            stat, pvalue = ttest_ind(group0, group1, equal_var=equal_variance)
+            test_used = "ttest_ind"
+        else:
+            stat, pvalue = ttest_ind(group0, group1, equal_var=equal_variance)
+            test_used = "ttest_welch"
+
+    elif feature_value_category in ("binary", "nominal"):
+        contingency = pd.crosstab(series, targets_series)
+        chi2, p_chi2, dof, expected = chi2_contingency(contingency)
+        if (expected < 5).any():
+            stat, pvalue = fisher_exact(contingency)
+            test_used = "fisher-exact"
+        else:
+            stat, pvalue = chi2, p_chi2
+            test_used = "chi-square"
+    else:
+        test_used = None
+        stat = None
+        pvalue = None
+
+    return test_used, stat, pvalue
+
+
+def check_normality(group0, group1, alpha):
+    _, pvalue0 = shapiro(group0)
+    _, pvalue1 = shapiro(group1)
+
+    return pvalue0 > alpha and pvalue1 > alpha
+
+
+def get_clinical_feature_univariate_type(series, feat_type, max_categories: int = 10):
+    n_unique = series.nunique()
+
+    if n_unique == 2:
+        return "binary"
+
+    if feat_type == ClinicalFeatureTypes.NUMBER.value:
+        if n_unique <= max_categories:
+            return "nominal"
+        return "continuous"
+
+    if n_unique <= max_categories:
+        return "nominal"
+
+    return "something_else"
